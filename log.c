b52d1b03006b573700df786b70e0e3d7344172bd
Luciano Maciel <lucianomaciel53@gmail.com>
2025-11-24 14:28:33 -0400

    Update log.c

--- log.c
+++ log.c
@@ -1,12 +1,12 @@
-#include "log.h"
 #include <stdio.h>
 #include <time.h>
+#include "log.h"
 
-void registrarLog(const char* mensagem) {
-    FILE* f = fopen("logs.txt", "a");
-    if (f) {
+void registrarLog(const char *mensagem) {
+    FILE *f = fopen("log.txt", "a");
+    if (f != NULL) {
         time_t agora = time(NULL);
-        fprintf(f, "%s - %s", mensagem, ctime(&agora));
+        fprintf(f, "%s - %s\n", ctime(&agora), mensagem);
         fclose(f);
     }
 }
