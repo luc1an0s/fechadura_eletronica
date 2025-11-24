06f10aaad450919527c701fd352971f05cf353f6
Luciano Maciel <lucianomaciel53@gmail.com>
2025-11-24 15:20:30 -0400

    Update log.c

--- log.c
+++ log.c
@@ -3,10 +3,16 @@
 #include "log.h"
 
 void registrarLog(const char *mensagem) {
-    FILE *f = fopen("log.txt", "a");
-    if (f != NULL) {
-        time_t agora = time(NULL);
-        fprintf(f, "%s - %s\n", ctime(&agora), mensagem);
-        fclose(f);
-    }
+    FILE *arquivo = fopen("log.txt", "a");
+    if (arquivo == NULL) return;
+
+    time_t agora = time(NULL);
+    struct tm *t = localtime(&agora);
+
+    fprintf(arquivo, "[%02d/%02d/%04d %02d:%02d:%02d] %s\n",
+            t->tm_mday, t->tm_mon+1, t->tm_year+1900,
+            t->tm_hour, t->tm_min, t->tm_sec,
+            mensagem);
+
+    fclose(arquivo);
 }
