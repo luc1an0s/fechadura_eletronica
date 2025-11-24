0e0188fe796ed08f1e67cb39d539361c9e8bcea5
Luciano Maciel <lucianomaciel53@gmail.com>
2025-11-24 16:46:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>  
 #include <string.h>
 #include <locale.h>
 #include "usuario.h"
 #include "serial.h"
 #include "log.h"
 
-// Cores ANSI
 #define RESET   "\033[0m"
 #define RED     "\033[1;31m"
 #define GREEN   "\033[1;32m"
 #define YELLOW  "\033[1;33m"
 #define BLUE    "\033[1;34m"
 #define CYAN    "\033[1;36m"
-
 
 void limparTela() {
 #ifdef _WIN32
@@ -35,7 +34,7 @@ void mostrarMenu() {
 
 void lerLinha(char *buffer, int tamanho) {
     if (fgets(buffer, tamanho, stdin)) {
-        buffer[strcspn(buffer, "\n")] = 0; 
+        buffer[strcspn(buffer, "\n")] = 0;
     }
 }
 
