342c0ea5bfadaee4a3d83e3fd8fe2b367c59ef3d
Luciano Maciel <lucianomaciel53@gmail.com>
2025-11-24 14:26:19 -0400

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,12 +1,18 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+#define MAX_USUARIOS 10
+#define MAX_SENHA 20
+
 typedef struct {
     char nome[50];
-    char senha[20];
+    char senha[MAX_SENHA];
 } Usuario;
 
-void cadastrarUsuario();
-void listarUsuarios();
+extern Usuario usuarios[MAX_USUARIOS];
+extern int totalUsuarios;
+
+void cadastrarUsuario(const char *nome, const char *senha);
+int validarUsuario(const char *senha);
 
 #endif
