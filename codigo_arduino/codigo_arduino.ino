9dcb42bae0660c969d10e2849af4f836df7ec245
Luc1an0s <lucianomaciel53@gmail.com>
2025-11-28 16:28:35 -0400

    update projeto

--- codigo_arduino/codigo_arduino.ino
+++ codigo_arduino/codigo_arduino.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
+#include <Servo.h>   
 
 #define MAX_USUARIOS 10
 #define BUZZER_PIN 10
+#define SERVO_PIN 11   
 
 struct Usuario {
   String nome;
@@ -32,6 +34,7 @@ byte colPins[COLS] = {5, 4, 3, 2};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 String senhaDigitada = "";
+Servo meuServo;   
 
 void setup() {
   Serial.begin(9600);
@@ -39,12 +42,14 @@ void setup() {
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Sistema pronto");
+
   pinMode(BUZZER_PIN, OUTPUT);
+  meuServo.attach(SERVO_PIN);   
+  meuServo.write(0);            
 }
 
 void loop() {
-
-  // Leitura do teclado
+  
   char key = keypad.getKey();
   if (key) {
     if (key == '#') {
@@ -65,10 +70,8 @@ void loop() {
     }
     else {
       senhaDigitada += key;
-
       lcd.clear();
       lcd.print("Senha: ");
-
       // Mostra **** no LCD, em vez da senha real
       for (int i = 0; i < senhaDigitada.length(); i++) {
         lcd.print("*");
@@ -78,7 +81,6 @@ void loop() {
 
   if (Serial.available()) {
     String comando = Serial.readStringUntil('\n');
-
     comando.replace("\r", "");  
     comando.trim();             
 
@@ -97,8 +99,14 @@ void validarSenha(String senha) {
       encontrado = true;
       lcd.clear();
       lcd.print("Acesso liberado");
-
       Serial.println("LOGIN:" + usuarios[i].nome + ";SUCESSO");
+
+      meuServo.write(90);   
+      delay(3000);         
+      meuServo.write(0);   
+      lcd.clear();
+      lcd.print("Sistema pronto");
+ 
       return;
     }
   }
@@ -110,11 +118,8 @@ void validarSenha(String senha) {
   }
 }
 
-
 void cadastrarUsuario(String comando) {
-
   comando.replace("CADASTRO:", "");
-
   int p1 = comando.indexOf(';');
   int p2 = comando.indexOf(';', p1+1);
 
@@ -122,12 +127,10 @@ void cadastrarUsuario(String comando) {
   String senha = comando.substring(p1+1, p2);
   String nivel = comando.substring(p2+1);
 
-
   for (int i = 0; i < totalUsuarios; i++) {
     if (usuarios[i].nome == nome) {
       usuarios[i].senha = senha;
       usuarios[i].nivel = nivel;
-
       Serial.println("USUARIO_ATUALIZADO:" + nome);
       lcd.clear();
       lcd.print("Senha alterada");
@@ -140,7 +143,6 @@ void cadastrarUsuario(String comando) {
     usuarios[totalUsuarios].senha = senha;
     usuarios[totalUsuarios].nivel = nivel;
     totalUsuarios++;
-
     Serial.println("USUARIO_CADASTRADO:" + nome);
     lcd.clear();
     lcd.print("Usuario criado");
