405cd1ee265a5f0aa9659a58f1c7580aa22bec61
Luciano Maciel <lucianomaciel53@gmail.com>
2025-12-09 10:10:09 -0400

    Update codigo_arduino.ino

--- codigo_arduino/codigo_arduino.ino
+++ codigo_arduino/codigo_arduino.ino
@@ -1,16 +1,22 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
-#include <Servo.h>   
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 #define MAX_USUARIOS 10
 #define BUZZER_PIN 10
-#define SERVO_PIN A0   
+#define SERVO_PIN A0
+
+#define SS_PIN 10
+#define RST_PIN A1
 
 struct Usuario {
   String nome;
   String senha;
   String nivel;
+  String uid; 
 };
 
 Usuario usuarios[MAX_USUARIOS];
@@ -18,7 +24,6 @@ int totalUsuarios = 0;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Teclado matricial
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
@@ -34,10 +39,34 @@ byte colPins[COLS] = {5, 4, 3, 2};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 String senhaDigitada = "";
-Servo meuServo;   
+Servo meuServo;
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void abrirPorta() {
+  meuServo.write(90);
+  delay(3000);
+  meuServo.write(0);
+}
+
+String lerUID() {
+  if (!rfid.PICC_IsNewCardPresent()) return "";
+  if (!rfid.PICC_ReadCardSerial()) return "";
+
+  String uid = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    uid += String(rfid.uid.uidByte[i], HEX);
+  }
+
+  rfid.PICC_HaltA();
+  return uid;
+}
 
 void setup() {
   Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
@@ -45,11 +74,28 @@ void setup() {
 
   pinMode(BUZZER_PIN, OUTPUT);
   meuServo.attach(SERVO_PIN);   
-  meuServo.write(0);            
+  meuServo.write(0);
 }
 
 void loop() {
-  
+
+  String uid = lerUID();
+  if (uid != "") {
+    for (int i = 0; i < totalUsuarios; i++) {
+      if (usuarios[i].uid == uid) {
+        lcd.clear();
+        lcd.print("RFID liberado");
+        Serial.println("LOGIN:" + usuarios[i].nome + ";RFID_OK");
+        abrirPorta();
+        lcd.clear();
+        lcd.print("Sistema pronto");
+        return;
+      }
+    }
+
+    Serial.println("RFID_DESCONHECIDO:" + uid);
+  }
+
   char key = keypad.getKey();
   if (key) {
     if (key == '#') {
@@ -61,61 +107,59 @@ void loop() {
       lcd.clear();
       lcd.print("Senha limpa");
     }
-    else if (key == 'A') {
-      tone(BUZZER_PIN, 1000);
-      delay(300);
-      noTone(BUZZER_PIN);
-      lcd.clear();
-      lcd.print("Buzzer acionado");
-    }
     else {
       senhaDigitada += key;
       lcd.clear();
       lcd.print("Senha: ");
-      // Mostra **** no LCD, em vez da senha real
-      for (int i = 0; i < senhaDigitada.length(); i++) {
-        lcd.print("*");
-      }
+      for (int i = 0; i < senhaDigitada.length(); i++) lcd.print("*");
     }
   }
 
   if (Serial.available()) {
     String comando = Serial.readStringUntil('\n');
-    comando.replace("\r", "");  
-    comando.trim();             
+    comando.replace("\r", "");
+    comando.trim();
 
     if (comando.startsWith("CADASTRO:")) {
       cadastrarUsuario(comando);
-      Serial.println("OK");     
+      Serial.println("OK");
+    }
+
+    if (comando == "LER_RFID") {
+      lcd.clear();
+      lcd.print("Aprox. cartao...");
+      String u = "";
+      while (u == "") {
+        u = lerUID();
+      }
+      Serial.println("RFID:" + u);
+      lcd.clear();
+      lcd.print("Sistema pronto");
+    }
+
+    if (comando.startsWith("CADASTRO_RFID:")) {
+      cadastrarUsuarioRFID(comando);
+      Serial.println("OK");
     }
   }
 }
 
 void validarSenha(String senha) {
-  bool encontrado = false;
-
   for (int i = 0; i < totalUsuarios; i++) {
     if (usuarios[i].senha == senha) {
-      encontrado = true;
       lcd.clear();
       lcd.print("Acesso liberado");
       Serial.println("LOGIN:" + usuarios[i].nome + ";SUCESSO");
-
-      meuServo.write(90);   
-      delay(3000);         
-      meuServo.write(0);   
+      abrirPorta();
       lcd.clear();
       lcd.print("Sistema pronto");
- 
       return;
     }
   }
 
-  if (!encontrado) {
-    lcd.clear();
-    lcd.print("Senha incorreta");
-    Serial.println("LOGIN:DESCONHECIDO;FALHA");
-  }
+  lcd.clear();
+  lcd.print("Senha incorreta");
+  Serial.println("LOGIN:DESCONHECIDO;FALHA");
 }
 
 void cadastrarUsuario(String comando) {
@@ -131,25 +175,39 @@ void cadastrarUsuario(String comando) {
     if (usuarios[i].nome == nome) {
       usuarios[i].senha = senha;
       usuarios[i].nivel = nivel;
-      Serial.println("USUARIO_ATUALIZADO:" + nome);
       lcd.clear();
       lcd.print("Senha alterada");
       return;
     }
   }
 
-  if (totalUsuarios < MAX_USUARIOS) {
-    usuarios[totalUsuarios].nome = nome;
-    usuarios[totalUsuarios].senha = senha;
-    usuarios[totalUsuarios].nivel = nivel;
-    totalUsuarios++;
-    Serial.println("USUARIO_CADASTRADO:" + nome);
-    lcd.clear();
-    lcd.print("Usuario criado");
-  }
-  else {
-    Serial.println("CADASTRO_FALHA:limite");
-    lcd.clear();
-    lcd.print("Limite atingido");
-  }
+  usuarios[totalUsuarios].nome = nome;
+  usuarios[totalUsuarios].senha = senha;
+  usuarios[totalUsuarios].nivel = nivel;
+  usuarios[totalUsuarios].uid   = "";
+  totalUsuarios++;
+
+  lcd.clear();
+  lcd.print("Usuario criado");
+}
+
+void cadastrarUsuarioRFID(String comando) {
+  comando.replace("CADASTRO_RFID:", "");
+  int p1 = comando.indexOf(';');
+  int p2 = comando.indexOf(';', p1+1);
+  int p3 = comando.indexOf(';', p2+1);
+
+  String nome = comando.substring(0, p1);
+  String senha = comando.substring(p1+1, p2);
+  String nivel = comando.substring(p2+1, p3);
+  String uid   = comando.substring(p3+1);
+
+  usuarios[totalUsuarios].nome = nome;
+  usuarios[totalUsuarios].senha = senha;
+  usuarios[totalUsuarios].nivel = nivel;
+  usuarios[totalUsuarios].uid   = uid;
+  totalUsuarios++;
+
+  lcd.clear();
+  lcd.print("User+RFID criado");
 }
